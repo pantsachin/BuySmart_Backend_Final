7c1b23d29aab94f804b312cdd3b5282789dfb915
Sachin Pant <sachinpant96@gmail.com>
2021-06-16 12:52:29 +0530

    fix: revamping models and routers

--- controllers/wishlist.controller.js
+++
@@ -1,4 +0,0 @@
-const { User } = require("../models/user.model.js");
-const { Cart } = require("../models/cart.model.js");
-const { WishList } = require("../models/wishlist.model.js");
-const { Product } = require("../models/product.model.js");
